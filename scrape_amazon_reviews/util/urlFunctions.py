64fdd109039371e3fbd0651eb5eace0ef2b00c4d
Parth Maniar <officialparthmaniar@gmail.com>
2020-09-02 13:44:36 +0530

    :fire: Force Download

--- scrape_amazon_reviews/util/urlFunctions.py
+++ scrape_amazon_reviews/util/urlFunctions.py
@@ -2,6 +2,10 @@ import requests
 
 url_prefix = "https://www.amazon"
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+# random user-agent
+
+from fake_useragent import UserAgent
+ua = UserAgent(cache=False, use_cache_server=False)
 
 def construst_reviews_URL(domain: str, product_id: str) -> str:
     """Constructs review URL.
@@ -19,5 +23,12 @@ def get_URL(url: str) -> str:
     Returns:
         The content fetched from remote url.
     """
-    content: str = requests.get(url,headers=headers)
+    user_agent = ua.random
+    while (True):
+        content: str = requests.get(url, headers={'User-Agent': user_agent})
+        if "api-services-support@amazon.com" in content.text:
+            user_agent = ua.random
+            continue
+        break
+        
     return content
