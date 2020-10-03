8dff383bd10ef71d95be3da469c79033d3056955
Parth Maniar <officialparthmaniar@gmail.com>
2020-10-04 01:23:44 +0530

    :wrench: data-hooks

--- scrape_amazon/util/scrape.py
+++ scrape_amazon/util/scrape.py
@@ -1,36 +1,36 @@
-from bs4 import BeautifulSoup
-import re
-import pandas as pd
-from .urlFunctions import get_URL
 import math
+import re
 
-from multiprocessing.dummy import Pool
-from multiprocessing import cpu_count
-
+import pandas as pd
+from bs4 import BeautifulSoup
 from p_tqdm import p_map
 
-def flatten(l): return [item for sublist in l for item in sublist]
+from .urlFunctions import get_URL
+
+
+def flatten(list):
+    return [item for sublist in list for item in sublist]
 
 
 def get_all_review_page_url(res: str) -> str:
-    productPage = BeautifulSoup(res.text, 'html.parser')
-    path: str = productPage.find(
-        "a", {"data-hook": "see-all-reviews-link-foot"})['href']
+    productPage = BeautifulSoup(res.text, "html.parser")
+    path: str = productPage.find("a", {"data-hook": "see-all-reviews-link-foot"})[
+        "href"
+    ]
     return path
 
 
 def extractPage(url: str) -> str:
     r = get_URL(url)
     pageNotLoaded = True
-    productPage = BeautifulSoup(r.text, 'html.parser')
+    productPage = BeautifulSoup(r.text, "html.parser")
     checkReviewLen = len(productPage.findAll("i", {"class": "review-rating"}))
     if checkReviewLen > 0:
         pageNotLoaded = False
     while pageNotLoaded:
         r = get_URL(url)
-        productPage = BeautifulSoup(r.text, 'html.parser')
-        checkReviewLen = len(productPage.findAll(
-            "i", {"class": "review-rating"}))
+        productPage = BeautifulSoup(r.text, "html.parser")
+        checkReviewLen = len(productPage.findAll("i", {"class": "review-rating"}))
         if checkReviewLen > 0:
             pageNotLoaded = False
     reviewers = []
@@ -39,12 +39,13 @@ def extractPage(url: str) -> str:
     reviewTitles = []
     reviewrsSpan = productPage.findAll("span", {"class": "a-profile-name"})
     ratingsSpan = productPage.findAll("i", {"class": "review-rating"})
-    reviewTitlesSpan = productPage.findAll(
-        "a", {"class": "review-title-content"})
+    reviewTitlesSpan = productPage.findAll("a", {"class": "review-title-content"})
     reviewDescriptionSpan = productPage.findAll(
-        "span", {"class": "review-text-content"})
+        "span", {"class": "review-text-content"}
+    )
 
-    # Loop is initiated from 2 because we have to exclude the Top Positive and Top Critical Review which otherwise will get repeated.
+    # Loop is initiated from 2 because we have to exclude the Top Positive and
+    # Top Critical Review which otherwise will get repeated.
     for i in range(2, len(reviewrsSpan)):
         reviewers.append(reviewrsSpan[i].get_text())
         ratings.append(int(ratingsSpan[i].get_text()[0]))
@@ -53,57 +54,60 @@ def extractPage(url: str) -> str:
         reviewTitles.append(reviewTitlesSpan[i].get_text())
         reviewDescriptions.append(reviewDescriptionSpan[i].get_text())
 
-    reviewDescriptions[:] = [i.lstrip('\n').rstrip(
-        '\n').strip() for i in reviewDescriptions]
-    reviewTitles[:] = [i.lstrip('\n').rstrip('\n') for i in reviewTitles]
+    reviewDescriptions[:] = [
+        i.lstrip("\n").rstrip("\n").strip() for i in reviewDescriptions
+    ]
+    reviewTitles[:] = [i.lstrip("\n").rstrip("\n") for i in reviewTitles]
 
-    return {"reviewers":reviewers, 
-            "ratings":ratings, 
-            "reviewTitles":reviewTitles, 
-            "reviewDescriptions":reviewDescriptions}
+    return {
+        "reviewers": reviewers,
+        "ratings": ratings,
+        "reviewTitles": reviewTitles,
+        "reviewDescriptions": reviewDescriptions,
+    }
 
 
 def extractTotalPages(url):
     r = get_URL(url)
-    productPage = BeautifulSoup(r.text, 'html.parser')
-    pageSpanText = productPage.find(
-        "span", {"data-hook": "cr-filter-info-review-count"}).get_text()
-    regex = re.compile('of ([0-9]*)')
-    totalReviews = int(regex.findall(pageSpanText.replace(",", ""))[0])
-    return math.ceil(totalReviews/10), productPage.find("title").get_text(), totalReviews
+    productPage = BeautifulSoup(r.text, "html.parser")
+    pageSpanText = productPage.findAll(
+        "span", {"class": "a-size-base a-color-secondary"}
+    )[0].get_text()
+    totalReviews = int(re.findall(r"\d+", pageSpanText.replace(",", ""))[0])
+    return (
+        math.ceil(totalReviews / 10),
+        productPage.find("title").get_text(),
+        totalReviews,
+    )
 
 
 def scrape_reviews(url):
-    totalReviewers = []
-    totalRatings = []
-    totalReviewDescriptions = []
-    totalReviewTitles = []
     totalPages, pageTitle, totalReviews = extractTotalPages(url)
     print(f"[scrape-amazon]  - {pageTitle}")
     print(f"[scrape-amazon] Total Pages - {totalPages}")
     print(f"[scrape-amazon] Total Reviews - {totalReviews}\n")
-    urlsToFetch =[]
-    for page in range(1, totalPages+1):
-        urlToFetch = url+f"?pageNumber={page}"
+    urlsToFetch = []
+    for page in range(1, totalPages + 1):
+        urlToFetch = url + f"?pageNumber={page}"
         urlsToFetch.append(urlToFetch)
 
     results = p_map(extractPage, urlsToFetch)
-    res = {} 
-    for k in results: 
-        for list in k: 
-            if list in res: 
-                res[list] += (k[list]) 
-            else: 
-                res[list] = k[list] 
+    res = {}
+    for k in results:
+        for list in k:
+            if list in res:
+                res[list] += k[list]
+            else:
+                res[list] = k[list]
 
     productReviewsData = pd.DataFrame()
 
     # # Adding Information
 
-    productReviewsData["Reviewer"] = res['reviewers']
-    productReviewsData["Rating"] = res['ratings']
-    productReviewsData["Title"] = res['reviewTitles']
-    productReviewsData["Description"] = res['reviewDescriptions']
+    productReviewsData["Reviewer"] = res["reviewers"]
+    productReviewsData["Rating"] = res["ratings"]
+    productReviewsData["Title"] = res["reviewTitles"]
+    productReviewsData["Description"] = res["reviewDescriptions"]
     # productReviewsData["link"] = url
     # productReviewsData["Product Title"] = pageTitle
 
