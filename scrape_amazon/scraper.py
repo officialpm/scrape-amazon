1c37e4a0adacc307ccdde9b939aea755bb607798
Parth Maniar <officialparthmaniar@gmail.com>
2020-10-04 01:27:14 +0530

    :recycle: removed unwanted imports and spaces

--- scrape_amazon/scraper.py
+++ scrape_amazon/scraper.py
@@ -1,9 +1,9 @@
 """Defines various scraper functions."""
-import sys
-
-from .util.urlFunctions import construst_reviews_URL, get_URL
-from .util.scrape import get_all_review_page_url, scrape_reviews
 from types import ModuleType
+
+from .util.scrape import scrape_reviews
+from .util.urlFunctions import construst_reviews_URL
+
 
 def get_reviews(domain: str, product_id: str) -> ModuleType:
     print(f"[INFO] Scraping Reviews of Amazon ProductID - {product_id}")
@@ -13,8 +13,5 @@ def get_reviews(domain: str, product_id: str) -> ModuleType:
     Returns:
         Scraped Dataframe
     """
-    all_reviews_url = construst_reviews_URL(domain,product_id)
+    all_reviews_url = construst_reviews_URL(domain, product_id)
     return scrape_reviews(all_reviews_url)
-    
-
-
